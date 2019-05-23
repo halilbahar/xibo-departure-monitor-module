9c66567aacae62cd7941996c12fb64a8f5c851c1
Halil Bahar <halil.s.bahar@gmail.com>
2019-05-23 09:52:37 +0200

    Install file now can copy the key into the code

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+#Replace key placeholder in the code with the actual key
+wienerLinienKey=xxxxxxxxxx
+sed -i "s/<Key für Wiener Linien>/$wienerLinienKey/g" ./DepartureMonitor/DepartureMonitor.php
+
 cp -r ./DepartureMonitor/ ../
 cp ./departuremonitor.json ../
